290a71db5975a2cbd8ebf75c3ef90d0f862682b6
Andrew Davis <abdavis7@gmail.com>
2022-12-05 01:45:41 -0700

    switched to using vec instead of vecdeque

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 const INPUT: &str = include_str!("inputs/day05.txt");
 pub fn run() -> String {
     let (crates, instructions) = parse_input::<9>(INPUT);
@@ -7,13 +5,13 @@ pub fn run() -> String {
 }
 fn parse_input<const NUM_STACKS: usize>(
     input: &str,
-) -> ([VecDeque<char>; NUM_STACKS], Vec<(u8, u8, u8)>) {
+) -> ([Vec<char>; NUM_STACKS], Vec<(u8, u8, u8)>) {
     let (crates_str, instructions_str) = input.split_once("\n\n").unwrap();
-    let mut crates = std::array::from_fn(|_| VecDeque::new());
-    for line in crates_str.lines() {
+    let mut crates = std::array::from_fn(|_| Vec::new());
+    for line in crates_str.lines().rev() {
         for (n, c) in line.chars().skip(1).step_by(4).enumerate() {
             match c {
-                c @ 'A'..='Z' => crates[n].push_back(c),
+                c @ 'A'..='Z' => crates[n].push(c),
                 _ => (),
             }
         }
@@ -30,34 +28,25 @@ fn parse_input<const NUM_STACKS: usize>(
     (crates, instructions)
 }
 fn part1<const NUM_STACKS: usize>(
-    mut crates: [VecDeque<char>; NUM_STACKS],
+    mut crates: [Vec<char>; NUM_STACKS],
     instructions: &Vec<(u8, u8, u8)>,
 ) -> String {
     for (moves, from, to) in instructions {
         for _ in 0..*moves {
-            let val = crates[*from as usize - 1].pop_front().unwrap();
-            crates[*to as usize - 1].push_front(val);
+            let val = crates[*from as usize - 1].pop().unwrap();
+            crates[*to as usize - 1].push(val);
         }
     }
-    crates
-        .iter_mut()
-        .map(|que| que.pop_front().unwrap())
-        .collect()
+    crates.iter_mut().map(|que| que.pop().unwrap()).collect()
 }
 fn part2<const NUM_STACKS: usize>(
-    mut crates: [VecDeque<char>; NUM_STACKS],
+    mut crates: [Vec<char>; NUM_STACKS],
     instructions: &Vec<(u8, u8, u8)>,
 ) -> String {
     for (moves, from, to) in instructions {
         let (moves, from, to) = (*moves as usize, *from as usize - 1, *to as usize - 1);
-        let mut other = crates[from].split_off(moves);
-        std::mem::swap(&mut other, &mut crates[from]);
-        for container in other.iter().rev() {
-            crates[to].push_front(*container);
-        }
+        let mut moving = crates[from].split_off(crates[from].len() - moves);
+        crates[to].append(&mut moving);
     }
-    crates
-        .iter_mut()
-        .map(|que| que.pop_front().unwrap())
-        .collect()
+    crates.iter_mut().map(|que| que.pop().unwrap()).collect()
 }
