17e2ecf3a80715dcd2f81a1f58d2e4c10464d0b6
Andrew Davis <abdavis7@gmail.com>
2022-12-26 20:06:22 -0700

    day16 part 2 is complete this solution is quite ugly, but it works.

--- src/day16.rs
+++ src/day16.rs
@@ -5,23 +5,31 @@ const EXAMPLE: &str = include_str!("example_inputs/day16.txt");
 pub fn run() -> String {
     //let test_graph: Graph<10> = EXAMPLE.into();
     let graph: Graph<59> = INPUT.into();
-    format!("{}", graph.part_1())
+    format!("{}\n{}", graph.part_1(), graph.part_2())
 }
 #[derive(Clone)]
 struct Worker {
     pos: usize,
     status: Status,
 }
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 enum Status {
     Ready,
-    working(usize),
+    Working(usize),
 }
 struct Graph<'a, const N: usize> {
     key: GraphKey<'a, N>,
     costs: GraphCosts<N>,
 }
 impl<const N: usize> Graph<'_, N> {
+    fn part_2(&self) -> usize {
+        let pos = self.key.names.iter().position(|n| n == &"AA").unwrap();
+        let start_state = Worker {
+            pos,
+            status: Status::Ready,
+        };
+        self.part_2_recurse(26, self.broken_valves(), start_state.clone(), start_state)
+    }
     fn part_2_recurse(
         &self,
         time_left: usize,
@@ -32,7 +40,10 @@ impl<const N: usize> Graph<'_, N> {
         if time_left == 0 {
             return 0;
         }
-        if valves_opened.trailing_ones() as usize == N {
+        if valves_opened.trailing_ones() as usize == N
+            && Status::Ready == me.status
+            && Status::Ready == elephant.status
+        {
             return 0;
         }
         match (&me.status, &elephant.status) {
@@ -49,7 +60,7 @@ impl<const N: usize> Graph<'_, N> {
                     }
                     let mut new_me = me.clone();
                     new_me.pos = m_j;
-                    new_me.status = Status::working(me_spent_time);
+                    new_me.status = Status::Working(me_spent_time);
                     let mut elephant_working = false;
                     for e_j in (0..m_j).chain(m_j + 1..N) {
                         if valves_opened & (1 << e_j) != 0 {
@@ -59,13 +70,151 @@ impl<const N: usize> Graph<'_, N> {
                         if eleph_spent_time >= time_left {
                             continue;
                         }
+                        let mut new_elephant = elephant.clone();
+                        new_elephant.pos = e_j;
+                        new_elephant.status = Status::Working(eleph_spent_time);
+                        max_release = max_release.max(self.part_2_recurse(
+                            time_left,
+                            valves_opened | (1 << m_j) | (1 << e_j),
+                            new_me.clone(),
+                            new_elephant,
+                        ));
+                        elephant_working = true;
+                        me_working = true;
+                    }
+                    if !elephant_working {
+                        //if the elephant can't do anything, go ahead with just me
+                        max_release = max_release.max(self.part_2_recurse(
+                            time_left,
+                            valves_opened | (1 << m_j),
+                            new_me,
+                            elephant.clone(),
+                        ));
+                        me_working = true;
+                    }
+                }
+                if !me_working {
+                    //if me didn't do anything, check what elephant can do
+                    for e_j in 0..N {
+                        if valves_opened & (1 << e_j) != 0 {
+                            continue;
+                        }
+                        let elephant_time_spent = self.costs.0[elephant.pos][e_j] as usize + 1;
+                        if elephant_time_spent >= time_left {
+                            continue;
+                        }
+                        let mut new_elephant = elephant.clone();
+                        new_elephant.pos = e_j;
+                        new_elephant.status = Status::Working(elephant_time_spent);
+                        max_release = max_release.max(self.part_2_recurse(
+                            time_left,
+                            valves_opened | (1 << e_j),
+                            me.clone(),
+                            new_elephant,
+                        ))
                     }
                 }
                 max_release
             }
-            (Status::Ready, Status::working(_)) => todo!(),
-            (Status::working(_), Status::Ready) => todo!(),
-            (Status::working(_), Status::working(_)) => todo!(),
+            (Status::Ready, Status::Working(_)) => {
+                let mut max_release = 0;
+                let mut progress_made = false;
+                for m_j in 0..N {
+                    if valves_opened & (1 << m_j) != 0 {
+                        continue;
+                    }
+                    let me_time_spent = self.costs.0[me.pos][m_j] as usize + 1;
+                    if me_time_spent >= time_left {
+                        continue;
+                    }
+                    let mut new_me = me.clone();
+                    new_me.pos = m_j;
+                    new_me.status = Status::Working(me_time_spent);
+                    max_release = max_release.max(self.part_2_recurse(
+                        time_left,
+                        valves_opened | (1 << m_j),
+                        new_me,
+                        elephant.clone(),
+                    ));
+                    progress_made = true;
+                }
+                if !progress_made {
+                    let mut new_elephant = elephant.clone();
+                    if let Status::Working(t) = elephant.status {
+                        new_elephant.status = Status::Ready;
+                        let new_time_left = time_left - t;
+                        return self.key.flow_rates[elephant.pos] as usize * new_time_left
+                            + self.part_2_recurse(new_time_left, valves_opened, me, new_elephant);
+                    }
+                }
+                max_release
+            }
+            (Status::Working(_), Status::Ready) => {
+                let mut max_release = 0;
+                let mut progress_made = false;
+                for e_j in 0..N {
+                    if valves_opened & (1 << e_j) != 0 {
+                        continue;
+                    }
+                    let elephant_time_spent = self.costs.0[elephant.pos][e_j] as usize + 1;
+                    if elephant_time_spent >= time_left {
+                        continue;
+                    }
+                    let mut new_elephant = elephant.clone();
+                    new_elephant.pos = e_j;
+                    new_elephant.status = Status::Working(elephant_time_spent);
+                    max_release = max_release.max(self.part_2_recurse(
+                        time_left,
+                        valves_opened | (1 << e_j),
+                        me.clone(),
+                        new_elephant,
+                    ));
+                    progress_made = true;
+                }
+                if !progress_made {
+                    let mut new_me = me.clone();
+                    if let Status::Working(t) = me.status {
+                        new_me.status = Status::Ready;
+                        let new_time_left = time_left - t;
+                        return self.key.flow_rates[me.pos] as usize * new_time_left
+                            + self.part_2_recurse(new_time_left, valves_opened, new_me, elephant);
+                    }
+                }
+                max_release
+            }
+            (Status::Working(me_time), Status::Working(elephant_time)) => {
+                let time_passed = me_time.min(elephant_time);
+                let me_time_left = me_time - time_passed;
+                let eleph_time_left = elephant_time - time_passed;
+                let new_time_left = time_left - time_passed;
+                let mut pressure_release = 0;
+                let new_me = if me_time_left == 0 {
+                    pressure_release += self.key.flow_rates[me.pos] as usize * new_time_left;
+                    Worker {
+                        pos: me.pos,
+                        status: Status::Ready,
+                    }
+                } else {
+                    Worker {
+                        pos: me.pos,
+                        status: Status::Working(me_time_left),
+                    }
+                };
+                let new_eleph = if eleph_time_left == 0 {
+                    pressure_release += self.key.flow_rates[elephant.pos] as usize * new_time_left;
+                    Worker {
+                        pos: elephant.pos,
+                        status: Status::Ready,
+                    }
+                } else {
+                    Worker {
+                        pos: elephant.pos,
+                        status: Status::Working(eleph_time_left),
+                    }
+                };
+                pressure_release
+                    + self.part_2_recurse(new_time_left, valves_opened, new_me, new_eleph)
+            }
         }
     }
     fn part_1(&self) -> usize {
@@ -183,7 +332,7 @@ impl<'a, const N: usize> From<&'a str> for GraphKey<'a, N> {
             graphkey.names[i] = parts.next().unwrap();
             let mut parts = parts.skip(3);
             graphkey.flow_rates[i] = parts.next().unwrap().parse().unwrap();
-            let mut parts = parts.skip(4);
+            let parts = parts.skip(4);
             graphkey.edges[i] = parts.collect();
         }
         graphkey
